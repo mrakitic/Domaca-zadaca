eeb45c3c19a12499b6fdd6f14c74083c285bb789
Borna <Borna@Borna-PC>
2018-06-04 14:00:31 +0200

    Komitanje vjezbe

---
+++ Domaci rad/src/domacirad/Vjezba06.java
@@ -0,0 +1,17 @@
+package domacirad;
+
+public class Vjezba06 {
+	//Program ispisuje sve parne brojeve od 1 do 67
+	
+		public static void main(String[] args) {
+			for(int i=1;i<67;i=i+1) {		
+			if(i%2==0) {
+				System.out.println(i + " je paran broj");
+			}else {
+				System.out.println();
+			}
+		}
+	}
+	}
+
+
